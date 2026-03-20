a4a7f7cd9b387b59569eef5f4cee79476d293111
Jiyeon <s2436@e-mirim.hs.kr>
2026-03-20 10:05:49 +0900

    counter HTML, JS

---
+++ 02_counter/main.js
@@ -0,0 +1,19 @@
+const numSpace = document.getElementById('numSpace');
+const plusButton = document.getElementsByClassName('plusButton')[0];
+plusButton.addEventListener("click", () =>{
+    num += 1;
+        numSpace.innerHTML = num;
+});
+
+/*
+let num = 0;
+    function plus(){
+        num += 1;
+        numSpace.innerHTML = num;
+    }
+
+    function minus(){
+        num -= 1;
+        numSpace.innerHTML = num;
+    }
+*/
