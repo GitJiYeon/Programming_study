abbcd7c471cf51610b632155f39e42b8beabcacd
Jiyeon <s2436@e-mirim.hs.kr>
2026-03-27 08:27:16 +0900

    counter +, +2, reset

--- 02_counter/main.js
+++ 02_counter/main.js
@@ -1,14 +1,23 @@
 const numSpace = document.getElementById('numSpace');
 const plusButton = document.getElementsByClassName('plusButton')[0];
-plusButton.addEventListener("click", () =>{
+let num = 0;
+
+
+const update = (value) =>{
+    num+=value;
+    numSpace.innerHTML = num;
+};
+const reset = (value) =>{
+    num = 0;
+    numSpace.innerHTML = num;
+};
+
+/* plusButton.addEventListener("click", () =>{
     num += 1;
         numSpace.innerHTML = num;
 });
+*/
 
-const plus = () =>{
-    num++;
-    numSpace.innerHTML = num;
-};
 /*
 let num = 0;
     function plus(){
