b9bc6b6812cf72b03d30f748b65e7428218876b3
Jiyeon <s2436@e-mirim.hs.kr>
2026-04-15 10:08:41 +0900

    TodoLListApp addTodo() 진행중

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './todoList.css';
 import TodoItemEmpty from './components/TodoItemEmpty.jsx';
 import Button from './components/Button.jsx'
@@ -6,8 +7,26 @@ import TodoHeader from './components/TodoHeader.jsx';
 import TodoAdder from './components/TodoAdder.jsx';
 import TodoItem from './components/TodoItem.jsx';
 import TodoList from './components/TodoList.jsx';
+class Todo{
+    constructor(text){
+        this.id = Date.now();   //할일 id: 고유의 값 == new Date().getTime()
+        this.text = text;       //할일의 내용
+        this.isCompleted = false; //할일 완료 여부
+
+    }
+
+
+}
+
 function TodoListApp(){
-    
+    const [todos, setTodos] = useState([]);
+    const addTodo = (text) => setTodos((todos) => [
+        //이전 todos복사
+        ...todos, 
+        //newTodo 만들어서
+        //뒤에 추가할것
+        new Todo(text)
+    ]);
     return(
         <div className="todo">
             <TodoHeader />
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,5 +1,8 @@
 import Button from './Button.jsx';
-export default function TodoAdder(){
+export default function TodoAdder({ addTodo }){
+    //submit이면 handlesubmit 실행
+    //handleSubmit
+    //text 받아서 addTodo(text)
     return(
         <form className="todo__form">
             <input className="todo__input" type="text" placeholder="Enter your tasks here"></input>
